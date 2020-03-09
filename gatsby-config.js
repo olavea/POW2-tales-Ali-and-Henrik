3d08b0027c4e3ae210c021820d6101dee3e5a9d8
Ola Vea <ola@olavea.com>
2020-03-09 18:25:53 +0100

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,39 @@
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-  },
   plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `reading POW! tales for Ali-and-Henrik`,
+        short_name: `POW! tales`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `book/images/UnicornLillian2.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: `@olavea/gatsby-theme-picture-book`,
+      options: {
+        basePath: `/`,
+        imagePath: `book/images`,
+        audioPath: `book/audio`,
+        infoLink: {
+          title: "POW! on GitHub",
+          url: `https://github.com/raae/pow-app.git`,
+        },
+      },
+    },
   ],
+  siteMetadata: {
+    // First page will have siteTile as page title,
+    // the other pages follow this configuration <page#> | <siteTitle>.
+    siteTitle: `reading POW! tales for Ali-and-Henrik`,
+    // Used for SEO
+    siteDescription: `reading POW! tales for Ali-and-Henrik at Oslo Norway`,
+    // Will be set on the <html /> tag
+    siteLanguage: `no`,
+    // Twitter Handle
+    author: `@OlaHolstVea`,
+  },
 }
